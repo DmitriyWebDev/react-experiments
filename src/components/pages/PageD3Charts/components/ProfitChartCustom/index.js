c5f8963c66ffe9d858a660563e41adf3d6e17755
DmitriyWebDev <d-man_real_akkaunt.ru@mail.ru>
2019-05-21 23:36:48 +0600

    Svg charts. Utils refactoring

--- src/components/pages/PageD3Charts/components/ProfitChartCustom/index.js
+++ src/components/pages/PageD3Charts/components/ProfitChartCustom/index.js
@@ -3,10 +3,11 @@ import * as d3 from "d3";
 import "./style.scss";
 import { data } from "./fakeApi";
 import {
+  setAnimatedViewToSvgPath,
   getOldDaysChartLines,
   countDaysLeftOffsets,
   computeDayHeight
-} from "./utils";
+} from "./utils/utils";
 
 class ProfitChartCustom extends React.Component {
   constructor(props) {
@@ -71,20 +72,7 @@ class ProfitChartCustom extends React.Component {
 
     // Set animated view for <path />
     const path = document.querySelector("#chart path");
-    const length = path.getTotalLength();
-    // Clear any previous transition
-    path.style.transition = path.style.WebkitTransition = "none";
-    // Set up the starting positions
-    path.style.strokeDasharray = length + " " + length;
-    path.style.strokeDashoffset = length;
-    // Trigger a layout so styles are calculated & the browser
-    // picks up the starting position before animating
-    path.getBoundingClientRect();
-    // Define our transition
-    path.style.transition = path.style.WebkitTransition =
-      "stroke-dashoffset 2s ease-in-out";
-    // Go!
-    path.style.strokeDashoffset = "0";
+    setAnimatedViewToSvgPath(path);
   }
 
   render() {
--- src/components/pages/PageD3Charts/components/ProfitChartCustom/utils/utils.js
+++ src/components/pages/PageD3Charts/components/ProfitChartCustom/utils/utils.js
@@ -79,3 +79,29 @@ export function computeDayHeight(days, chartHeight, dayIndex) {
 
   return (chartHeight / 100) * dayProductsPercents * -1;
 }
+
+export function setAnimatedViewToSvgPath(pathNode = {}) {
+  if (typeof pathNode.getTotalLength === "undefined") return false;
+
+  // Set animated view for <path />
+  const path = pathNode;
+  const length = path.getTotalLength();
+
+  // Clear any previous transition
+  path.style.transition = path.style.WebkitTransition = "none";
+
+  // Set up the starting positions
+  path.style.strokeDasharray = length + " " + length;
+  path.style.strokeDashoffset = length;
+
+  // Trigger a layout so styles are calculated & the browser
+  // picks up the starting position before animating
+  path.getBoundingClientRect();
+
+  // Define our transition
+  path.style.transition = path.style.WebkitTransition =
+    "stroke-dashoffset 2s ease-in-out";
+
+  // Go!
+  path.style.strokeDashoffset = "0";
+}
